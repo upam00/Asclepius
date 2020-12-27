8aba0d31ce7992733c1b9c32e3431944066a5210
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2020-12-28 02:32:06 +0530

    sql files for inserting dummy data

---
+++ SQL files/sql files for inserting dummy data/6_insert_patients.sql
@@ -0,0 +1,12 @@
+INSERT INTO Patient (Patient_ID, F_Name, M_Name, L_Name, Age, Sex, Patient_Type, Check_in_Receptionist_ID, Assigned_Doc_ID)
+	VALUES
+		(1, 'Rabijit', NULL, 'Singh', 9, 'M', 'In Patient', 4, 1),
+		(2, 'Ranveer', 'Kapoor', 'Singh', 50, 'M', 'Out patient', 5, 1),
+		(3, 'Katrina', NULL, 'Kaif', 30, 'F', 'In Patient', 6, 1),
+		(4, 'Deepika', NULL, 'Padukone', 28, 'F', 'Out Patient', 4, 2),
+		(5, 'Gautam', NULL, 'Das', 16, 'M', 'In Patient', 5, 2),
+		(6, 'Ranbir', NULL, 'Kapoor', 30, 'M', 'In Patient', 6, 2),
+		(7, 'Yui', NULL, 'Hanazawa', 24, 'F', 'In Patient', 4, 3),
+		(8, 'Levi', NULL, 'Ackerman', 32, 'M', 'Out Patient', 5, 3),
+		(9, 'Eren', NULL, 'Yeager', 30, 'M', 'Out Patient', 6, 3),
+		(10, 'Yuuji', NULL, 'Itadori', 17, 'M', 'Out Patient', 4, 1);
